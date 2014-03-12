8db770dfc2233f7092f9eb9cc11de6f8306ab231
WvanWoerden <wesselvanwoerden@gmail.com>
2014-03-12 18:18:02 +0100

    Graph, Basic Search, Breadcrumbs and Spinner
    Graph, Basic Search, Breadcrumbs and Loading spinner working

---
+++ sigmajs/graph.js
@@ -0,0 +1,429 @@
+(function() {
+  'use strict';
+
+  var numResults = 24;
+    // Read a page's GET URL variables and return them as an associative array.
+    function getUrlVars()
+    {
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++)
+        {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = decodeURIComponent(hash[1]);
+        }
+        return vars;
+    }
+
+    function wrapText(context, text, x, y, maxWidth, lineHeight) {
+        var cars = text.split("\n");
+
+        for (var ii = 0; ii < cars.length; ii++) {
+
+            var line = "";
+            var words = cars[ii].split(" ");
+
+            for (var n = 0; n < words.length; n++) {
+                var testLine = line + words[n] + " ";
+                var metrics = context.measureText(testLine);
+                var testWidth = metrics.width;
+
+                if (testWidth > maxWidth) {
+                    context.fillText(line, x, y);
+                    line = words[n] + " ";
+                    y += lineHeight;
+                }
+                else {
+                    line = testLine;
+                }
+            }
+
+            context.fillText(line, x, y);
+            y += lineHeight;
+        }
+    }
+
+    function wrapText2(context, text, x, y, maxWidth, lineHeight) {
+      var lines = [];
+      var line = "";
+      var words = text.split(" ");
+
+      for (var n = 0; n < words.length; n++) {
+        var testLine = line + words[n] + " ";
+        var metrics = context.measureText(testLine);
+        var testWidth = metrics.width;
+
+        if (testWidth > maxWidth && line != "") {
+          lines.push(line);
+          line = words[n] + " ";
+        }
+        else {
+          line = testLine;
+        }
+      }
+
+      lines.push(line);
+
+      y += lineHeight/4;
+      y -= (lines.length-1)/2 * lineHeight;
+
+      for(var i = 0; i < lines.length; i++){
+        context.fillText(lines[i], x, y);
+        y += lineHeight;
+      }
+
+    }
+
+    sigma.classes.camera.prototype.returnPosition = function(){
+      return {'x' : this.x, 'y' : this.y};
+    }
+    sigma.canvas.hovers.goo = function(node, context, settings) {
+
+    }
+
+    sigma.canvas.hovers.rel = function(node, context, settings) {
+
+    }
+
+    sigma.canvas.labels.goo = function(node, context, settings) {
+      var fontSize,
+          prefix = settings('prefix') || '',
+          size = node[prefix + 'size'];
+
+      context.textAlign = 'center';
+
+      if (size < settings('labelThreshold'))
+        return;
+
+      if (typeof node.label !== 'string')
+        return;
+
+      fontSize = (settings('labelSize') === 'fixed') ?
+        settings('defaultLabelSize') :
+        settings('labelSizeRatio') * size / 1.5;
+
+      context.font = (settings('fontStyle') ? settings('fontStyle') + ' ' : '') +
+        (fontSize*2) + 'px ' + settings('font');
+      context.fillStyle = '#ffffff';
+
+      /*(settings('labelColor') === 'node') ?
+        (node.color || settings('defaultNodeColor')) :
+        settings('defaultLabelColor');*/
+
+        wrapText(context, node.label, Math.round(node[prefix + 'x']), Math.round(node[prefix + 'y'] + fontSize / 3), 2*size, fontSize*2);
+      context.textAlign = 'left';
+
+    };
+
+    sigma.canvas.labels.rel = function(node, context, settings) {
+      var fontSize,
+          prefix = settings('prefix') || '',
+          size = node[prefix + 'size'];
+
+      context.textAlign = 'center';
+
+      if (size < settings('labelThreshold'))
+        return;
+
+      if (typeof node.label !== 'string')
+        return;
+
+      fontSize = (settings('labelSize') === 'fixed') ?
+        settings('defaultLabelSize') :
+        settings('labelSizeRatio') * size;
+
+      context.font = (settings('fontStyle') ? settings('fontStyle') + ' ' : '') +
+        (fontSize*2) + 'px ' + settings('font');
+      context.fillStyle = '#000';
+
+      /*(settings('labelColor') === 'node') ?
+        (node.color || settings('defaultNodeColor')) :
+        settings('defaultLabelColor');*/
+
+        wrapText2(context, node.label, Math.round(node[prefix + 'x']), Math.round(node[prefix + 'y'] + fontSize / 3), 2*size, fontSize*2);
+      context.textAlign = 'left';
+
+    };
+
+
+    sigma.canvas.edges.goo = function(e, s, t, ctx, settings) {
+      var color = e.color,
+          p = settings('prefix') || '',
+          edgeColor = settings('edgeColor'),
+          defaultNodeColor = settings('defaultNodeColor'),
+          defaultEdgeColor = settings('defaultEdgeColor'),
+          v,
+          d,
+          p1 = 5 / 6,
+          p2 = 1 / 6;
+
+      if (!color)
+        switch (edgeColor) {
+          case 'source':
+            color = s.color || defaultNodeColor;
+            break;
+          case 'target':
+            color = t.color || defaultNodeColor;
+            break;
+          default:
+            color = defaultEdgeColor;
+            break;
+        }
+
+      d = Math.sqrt(Math.pow(t[p + 'x'] - s[p + 'x'], 2) + Math.pow(t[p + 'y'] - s[p + 'y'], 2)) * 10;
+      v = {
+        x: (t[p + 'x'] - s[p + 'x']) / d,
+        y: (t[p + 'y'] - s[p + 'y']) / d
+      };
+
+      ctx.fillStyle = color;
+      ctx.beginPath();
+      ctx.moveTo(
+        s[p + 'x'] + v.y * s[p + 'size'],
+        s[p + 'y'] - v.x * s[p + 'size']
+      );
+      ctx.bezierCurveTo(
+        s[p + 'x'] * p1 + t[p + 'x'] * p2 + v.y * e[p + 'size'],
+        s[p + 'y'] * p1 + t[p + 'y'] * p2 - v.x * e[p + 'size'],
+        t[p + 'x'] * p1 + s[p + 'x'] * p2 + v.y * e[p + 'size'],
+        t[p + 'y'] * p1 + s[p + 'y'] * p2 - v.x * e[p + 'size'],
+        t[p + 'x'] + v.y * t[p + 'size'],
+        t[p + 'y'] - v.x * t[p + 'size']
+      );
+      ctx.lineTo(
+        t[p + 'x'] - v.y * t[p + 'size'],
+        t[p + 'y'] + v.x * t[p + 'size']
+      );
+      ctx.bezierCurveTo(
+        t[p + 'x'] * p1 + s[p + 'x'] * p2 - v.y * e[p + 'size'],
+        t[p + 'y'] * p1 + s[p + 'y'] * p2 + v.x * e[p + 'size'],
+        s[p + 'x'] * p1 + t[p + 'x'] * p2 - v.y * e[p + 'size'],
+        s[p + 'y'] * p1 + t[p + 'y'] * p2 + v.x * e[p + 'size'],
+        s[p + 'x'] - v.y * s[p + 'size'],
+        s[p + 'y'] + v.x * s[p + 'size']
+      );
+      ctx.closePath();
+      ctx.fill();
+    };
+
+    sigma.canvas.nodes.goo = function(node, ctx, settings) {
+      var prefix = settings('prefix') || '';
+
+      ctx.fillStyle = node.color || settings('defaultNodeColor');
+      ctx.beginPath();
+      ctx.arc(
+        node[prefix + 'x'],
+        node[prefix + 'y'],
+        node[prefix + 'size'],
+        0,
+        Math.PI * 2,
+        true
+      );
+      ctx.closePath();
+      ctx.fill();
+
+      /*
+      ctx.fillStyle = '#fff';
+      ctx.beginPath();
+      ctx.arc(
+        node[prefix + 'x'],
+        node[prefix + 'y'],
+        node[prefix + 'size'] * 0.5,
+        0,
+        Math.PI * 2,
+        true
+      );
+      ctx.closePath();
+      ctx.fill();
+      */
+
+    };
+
+  sigma.settings.maxNodeSize = 100;
+  sigma.settings.labelSize = "proportional";
+  sigma.settings.labelSizeRatio = 0.2;
+  sigma.settings.defaultLabelSize = 12;
+  sigma.settings.nodesPowRatio = 1;
+  sigma.settings.edgesPowRatio = 1;    
+  //sigma.settings.autoRescale = false;
+  sigma.settings.mouseEnabled = true;
+  sigma.settings.touchEnabled = false;
+  sigma.settings.zoomMin = .5;
+  sigma.settings.zoomMax = 1;
+
+  var breadcrumbs = function() {
+    var crumbs, ul;
+
+    Object.defineProperty(this, 'crumbs', {
+      value: [],
+      writable: true
+    });
+
+    Object.defineProperty(this, 'ul', {
+      value: $('.crumbs')[0]
+    });
+
+  }
+
+  breadcrumbs.prototype.add = function(topic) {
+    this.crumbs.push(topic);
+  };
+
+  breadcrumbs.prototype.removeFrom = function(from) {
+    this.crumbs = this.crumbs.slice(0, from+1);
+  };
+
+  breadcrumbs.prototype.clear = function(from) {
+    this.crumbs = [];
+  };
+
+  breadcrumbs.prototype.refresh = function() {
+    var _self = this;
+    var a = this.crumbs.length;
+    $(this.ul).children().remove();
+    if( a > 0 ){
+      $(this.ul).append(function() {
+        var $new = $('<li class="first"><a href="#" style="z-index:100;"><span></span>'+_self.crumbs[0]+'</a></li>');
+        $new.click({'index': 0, '_self' : _self }, _self.click);
+        return $new;
+      });
+      for( var i = 1; i < a; i++){
+        $(this.ul).append(function(){
+          return $('<li><a href="#" style="z-index:'+(100-i)+';"><span></span>'+_self.crumbs[i]+'</a></li>').click({'index': i, '_self' : _self }, _self.click);
+        });
+      }
+    }
+  }
+
+  breadcrumbs.prototype.getTopic = function(index){
+    if(typeof this.crumbs[index] != 'undefined' ) 
+      return this.crumbs[index];
+    else
+      return false;
+  }
+
+  breadcrumbs.prototype.click = function(event) {
+    var index = event.data.index;
+    var _self = event.data._self;
+    console.log(index);
+    var value = _self.getTopic(index);
+    if(value !== false) {
+      _self.removeFrom( index );
+      drawGraph( value );
+      _self.refresh();
+    }
+  }
+
+  var s;
+  function drawGraph(topic) {
+
+    $('#container').addClass('csspinner').addClass('traditional');
+
+    s.graph.clear();
+    s.graph.addNode({
+      // Main attributes:
+      id: 'm',
+      label: topic.replace(/\_/g,' '),
+      // Display attributes:
+      x: 0,
+      y: 0,
+      size: 2,
+      color: '#000',
+      type: 'goo'
+    });
+
+    // Finally, let's ask our sigma instance to refresh:
+    s.refresh();
+
+    var terms = [];
+    $.getJSON('http://newsbyiris.com/sigmajs/wikiMinerApiProxy.php?q='+topic, function(terms, textStatus) {
+      
+      var results = Math.min(terms.length, numResults);
+      for( var i = 0; i < results ; ++i ){
+        s.graph.addNode({
+          id: 'n'+terms[i].id,
+          label: terms[i].title,
+          x: (.5*((i+1)%2)+.2*(i%4==1)+.6) * Math.sin( 2 * Math.PI / results * i ),
+          y: (.5*((i+1)%2)+.2*(i%4==1)+.6) * Math.cos( 2 * Math.PI / results * i ),
+          size: terms[i].weight,
+          color: '#BBB',
+          type: 'rel'
+        })
+        .addEdge({
+          id: 'e'+i,
+          source: 'm',
+          target: 'n'+terms[i].id,
+          color: '#00f',
+          type: 'goo'
+        });
+
+      }
+
+      // Finally, let's ask our sigma instance to refresh:
+      s.refresh(); 
+
+      s.unbind('clickNode');
+
+      // Bind the events:
+      s.bind('clickNode', function(e) {
+        //console.log(e.data.node.label);
+        if(e.data.node.type == 'rel'){
+          c.add(e.data.node.label);
+          c.refresh();
+          drawGraph(e.data.node.label);
+        }
+        else
+          window.open('http://en.wikipedia.org/wiki/'+e.data.node.label.replace(/\ /g,'_'));
+      });
+
+      $('#container').removeClass('csspinner').removeClass('traditional');
+    });
+
+  }
+
+  var c;
+
+  $(document).ready(function($) {
+
+    c = new breadcrumbs();
+    c.refresh();
+
+    
+
+    // Let's first initialize sigma:
+    s = new sigma({
+      renderer: {
+        container: document.getElementById('container'),
+        type: 'canvas'
+      },
+    });
+
+    if(typeof getUrlVars().q != 'undefined'){
+      c.add(getUrlVars().q);
+      c.refresh();
+      drawGraph(getUrlVars().q);
+    }
+    
+
+    $('#searchSubmit').click(function(event) {
+      console.log('test2');
+      var value = $('#searchInput').val();
+
+      if(value != ''){
+        c.clear();
+        c.add(value);
+        c.refresh();
+        drawGraph(value);
+      }
+
+      
+    });
+
+    //c.click(1);
+
+  });
+
+
+    
+})();
